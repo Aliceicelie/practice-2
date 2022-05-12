7c7e8d2f761ce5248c31fa78861533c2e174edd6
Aliceicelie <aliceicelie@gmail.com>
2022-05-13 00:45:34 +0300

    created logic of adding new boxes

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const kirby = document.getElementById('kirby')
 const content = document.getElementById('boxForContainers')
+const hideBox1 = document.getElementById('hideBox1')
+const hideBox2 = document.getElementById('hideBox2')
 
-kirby.addEventListener('click', function swapImg()  {
+
+kirby.addEventListener('click', swapImg)
+
+function swapImg()  {
     if (kirby.src.match('logos/kirby_logo.png')) {
         kirby.src = 'logos/kirby_logo2.png'
+        addBoxes()
     } else {
         kirby.src = 'logos/kirby_logo.png'
+        removeBoxes()
     }
-})
+}
+
+let addBoxes = () => {
+    content.innerHTML += `
+    <div class="scndContainer" id="hideBox1">
+        <h1>CEO of Age International</h1>
+            <p>New York, Green Street</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, fuga. Tempora, delectus iste? Laborum incidunt cupiditate neque deserunt! Voluptatum quisquam ipsum perferendis quae atque deleniti maxime magnam facilis consequuntur sed.</p>
+    </div>
+
+    <div class="firstContainer" id="hideBox2">
+        <h1>Set Up</h1>
+            <p>Jacksonville</p>
+    </div>
+`
+}
+
+function removeBoxes() {
+    
+}
